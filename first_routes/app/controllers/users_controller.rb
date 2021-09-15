5da4b914ea0d167d1e62fcf743a7d85a3cb5583b
Suzi <lisuzanne94@gmail.com>
2021-09-15 17:21:56 -0400

    Finish first routes project

--- first_routes/app/controllers/users_controller.rb
+++ first_routes/app/controllers/users_controller.rb
@@ -1,16 +1,46 @@
+require 'byebug'
+
 class UsersController < ApplicationController
     def index
-        # @users = User.all
-        render plain: "I'm in the index action!"
+        @users = User.all
+        render json: @users
     end
 
     def create
-        render json: params
+        @user = User.new(user_params)
+        if @user.save
+            render json: @user
+        else 
+            render json: @user.errors.full_messages, status: :unprocessable_entity
+        end
     end
 
     def show
-        render json: params
+        @user = User.find(params[:id])
+        render json: @user
     end
 
-    
+    def update 
+        @user = User.find(params[:id])
+        # debugger
+        if @user.update(user_params)
+            # debugger
+            redirect_to user_url(@user) #user_url doesnt work?
+        else
+            render json: @user.errors.full_messages, status: :unprocessable_entity 
+        end
+    end
+
+    def destroy
+        @user = User.find(params[:id])
+        @user.destroy
+        redirect_to users_url
+    end
+
+
+    private
+
+    def user_params
+        params.require(:user).permit(:name, :email)
+    end
 end
